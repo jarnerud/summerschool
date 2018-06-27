03fbbc31fa316b2c19cbae2ff3146eba617848b9
Tova <jarnerud@kth.se>
2018-06-27 12:07:59 +0300

    c exercise 3a

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -7,5 +7,18 @@ int main(int argc, char *argv[])
     // write the control structure as described in the exercise
     // and run the code with different values for i
 
+    i =1115;
+
+    if(i<0){
+    	printf("i is less than zero, it is %d",i);
+    } else if(i==0){
+	printf("i is zero");
+    } else if (i>100){
+	printf("i is larger than 100, it is %d",i);
+    } else { 
+	printf("i is something else, it is %d",i);
+	}
+
+
     return 0;
 }
