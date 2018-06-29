91bb267b8d86310da5d11fad32ead70697c15f72
Tova <jarnerud@kth.se>
2018-06-29 11:51:41 +0300

    Exc OPENMP 2

--- openmp/data-sharing/variables.c
+++ openmp/data-sharing/variables.c
@@ -3,16 +3,33 @@
 int main(void)
 {
     int var1 = 1, var2 = 2;
-
-    /* TODO:
-     *   Test the effect of different data sharing clauses here
-     */
+	
+	#pragma omp parallel private (var1, var2)
+	
+	{
+	printf("Region 1: var1=%i, var2=%i\n", var1, var2);
+	var1++;
+	var2++;
+	}
+    printf("After region 1: var1=%1, var2=%i\n\n", var1, var2);
+	
+    #pragma omp parallel firstprivate (var1, var2)
     {
-        printf("Region 1: var1=%i, var2=%i\n", var1, var2);
+        printf("Region 2: var1=%i, var2=%i\n", var1, var2);
         var1++;
         var2++;
     }
-    printf("After region 1: var1=%i, var2=%i\n\n", var1, var2);
+    printf("After region 2: var1=%i, var2=%i\n\n", var1, var2);
+    #pragma omp parallel
+	{
+	printf("Region 3: var1=%i, var2=%i\n", var1, var2);
+	var1++;
+	var2++;
+	}
+	printf("After region 3: var1%i, var2=%i\n\n", var1, var2);
 
+	
     return 0;
 }
+/*Cannot use private if you need the value of var1, var2 from outside*/
+
