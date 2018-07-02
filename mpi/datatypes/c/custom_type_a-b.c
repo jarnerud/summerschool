10430eafab31266619a50ad18a200c7656f72065
Tova <jarnerud@kth.se>
2018-07-02 11:36:14 +0300

    Exc MPI IV 9b

--- mpi/datatypes/c/custom_type_a-b.c
+++ mpi/datatypes/c/custom_type_a-b.c
@@ -6,7 +6,9 @@ int main(int argc, char **argv)
     int rank;
     int array[8][8];
     //TODO: Declare a variable storing the MPI datatype
-	MPI_Datatype columntype;
+	MPI_Datatype indexedtype;
+	int displs[4];
+	int counts[4];
 
     int i, j;
 
@@ -37,16 +39,24 @@ int main(int argc, char **argv)
             printf("\n");
         }
     }
-
+	counts[0]= 1;
+	counts[1]= 2;
+	counts[2]= 3;
+	counts[3]= 4;
+	displs[0]= 0;
+	displs[1]= 17;
+	displs[2]= 34;
+	displs[3]= 51;
     //TODO: Create datatype that describes one column. Use MPI_Type_vector.
-	MPI_Type_vector(8, 1, 8, MPI_INT, &columntype);
-	MPI_Type_commit(&columntype);
+	MPI_Type_indexed(4, counts, displs, MPI_INT, &indexedtype);
+	MPI_Type_commit(&indexedtype);
 
     //TODO: Send first column of matrix form rank 0 to rank 1
 	if (rank==0) {
-		MPI_Send(&array[0][1], 1, columntype, 1, 1,MPI_COMM_WORLD);
+		MPI_Send(&array[0][0], 1, indexedtype, 1, 
+1,MPI_COMM_WORLD);
 }	else if (rank==1) {
-	MPI_Recv(&array[0][1], 1, columntype, 0, 1, MPI_COMM_WORLD, 
+	MPI_Recv(&array[0][0], 1, indexedtype, 0, 1, MPI_COMM_WORLD, 
 MPI_STATUS_IGNORE);
 	}
     //TODO: free datatype
@@ -62,7 +72,7 @@ MPI_STATUS_IGNORE);
             printf("\n");
         }
     }
-	MPI_Type_free(&columntype);
+	MPI_Type_free(&indexedtype);
     MPI_Finalize();
 
     return 0;
